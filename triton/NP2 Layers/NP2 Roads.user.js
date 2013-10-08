2f764271ba1c56c50e7bc59a981aa227638ce56e
AnnanFay <annanfay@gmail.com>
2013-10-08 22:11:00 +0100

    ...

--- triton/NP2 Layers/NP2 Roads.user.js
+++ triton/NP2 Layers/NP2 Roads.user.js
@@ -8,14 +8,7 @@
 // @grant       none
 // ==/UserScript==
 
-// Start userscript at document-start
-// Capture scripts with beforescriptexecute
-// If the script is one which we want to patch:
-//      get the contents with SYNCRONOUS http request
-//      patch contents
-//      inject into new script tag
-
-/* globals unsafeWindow, Mousetrap, map*/
+/* globals unsafeWindow */
 try {
     (function (w) {
         "strict true";
@@ -35,138 +28,211 @@ try {
             return (this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
         };
 
-        function get (page, callback, async) {
-            function listener () {
-                callback(this.responseText);
-            }
+        function noop () {}
 
-            var r = new XMLHttpRequest();
-            r.onload = listener;
-            r.open("get", page, (async === undefined ? false : async));
-            r.send();
-        }
-        
-        function patch (f) {
-            return f.toString().replace(/^function.*{|}$/g, '');
+        function to_array (a) {
+            return Array.prototype.slice.call(a, 0);
         }
 
-        function patch_script (source, start_tag, end_tag, new_code) {
-            function idx (c, t) {
-                debug('idx', arguments);
-                if (t instanceof RegExp) {
-                    var m = c.match(t);
-                    return m ? m.index : -1;
+        function partial (f) {
+            var args = to_array(arguments).slice(1);
+            return function () {
+                return f.apply(this, args.concat(to_array(arguments)));
+            };
+        }
+
+        function caller (f) {
+            var args = to_array(arguments).slice(1);
+            return function () {
+                return f.apply(this, args);
+            };
+        }
+
+        function wrap (f, pre, post) {
+            var w = function wrapped_function () {
+                var args = arguments;
+                if (pre) {
+                    args = pre(args);
                 }
-                return c.indexOf(t);
-            }
-            debug("PATCHING", source.length, start_tag, end_tag, new_code);
-            var start_tag_pos = idx(source, start_tag);
-            if (start_tag_pos === -1) {
-                debug("Could not find start tag", start_tag_pos, start_tag);
-                return "";
-            }
-
-            var start_pos    = start_tag_pos + start_tag.length;
-            var block_length = idx(source.slice(start_pos), end_tag);
-            if (block_length === -1) {
-                debug("Could not find end tag");
-                debug("Near code:", (source.slice(start_pos, start_pos+400)))
-                return "";
-            }
-
-            //debug("poses", start_tag_pos, start_pos, block_length);
-            var code = source.splice(start_pos, block_length, new_code);
-            debug('patched', code.slice(start_pos-20, start_pos+(new_code.length)+20));
-            return code;
+                var result = f.apply(this, args);
+                if (!post) {
+                    return result;
+                }
+                return post(args, result);
+            };
+            
+            return w;
         }
 
-        function disable_script (r, callback) {
-            function listener (e) {
-                var target = e.target;
-                var src = target.src;
-                var content = target.innerHTML;
+        function canvas (width, height) {
+            var c = document.createElement('canvas');
+            c.width = width;
+            c.height = height;
+            return c;
+        }
 
-                //debug("listening", src);
-                if ((src && !r.test(src)) || (content &&  !r.test(content))) {
+
+
+        function clearCircle (ctx, x, y, radius) {
+            ctx.save();
+            ctx.beginPath();
+            ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+            ctx.clip();
+            ctx.clearRect(x - radius - 1, y - radius - 1,
+                              radius * 2 + 2, radius * 2 + 2);
+            ctx.restore();
+        }
+
+        function tint (image, colour) {
+            var buffer = canvas(image.width, image.height),
+                bctx = buffer.getContext('2d');
+
+            bctx.fillStyle = colour;
+            bctx.fillRect(0, 0, buffer.width, buffer.height);
+            
+            bctx.globalCompositeOperation = "destination-atop";
+            bctx.drawImage(image, 0, 0);
+
+            return buffer;
+        }
+
+        // hook into script loading and append a call to _init() after jQuery.ready is called.
+        function inject_init () {
+            var injection_code = '_init(Mousetrap, Crux, NeptunesPride, universe, inbox, npui, npuis, np, si);\n';
+
+            function insert_script (content) {
+                content = content.splice(content.lastIndexOf('});'), 0, injection_code);
+                var s = document.createElement('script');
+                s.innerHTML = content;
+                document.head.appendChild(s);
+            }
+
+            function script_handler (e) {
+                var content = e.target.innerHTML;
+                if (content.indexOf('$(window).ready(function () {') === -1) {
                     return;
                 }
-
-                //debug("heard", src);
                 e.preventDefault();
                 e.stopPropagation();
-                
-                w.removeEventListener('beforescriptexecute', listener);
-                callback(src, content);
+                w.removeEventListener('beforescriptexecute', script_handler);
+
+                insert_script(content);
             }
 
-            w.addEventListener('beforescriptexecute', listener);
+            w.addEventListener('beforescriptexecute', script_handler);
         }
 
-        function insert_script (source) {
-            debug("inserting source");
-            var s = document.createElement('script');
-            s.innerHTML = source;
-              document.head.appendChild(s);
-              debug("inserted", s);
-        }
-        //
-        // Patches
-        //
-        var draw_star_patch = patch(function() {
-            var i, stars = universe.galaxy.stars;
-            if (universe.editMode !== "show_paths") {
-                for (i in stars) if (stars.hasOwnProperty(i)) {
-                    map.drawSprite(stars[i].sprite);
-                }
-                return;
+        function drawPath (ctx, starA, starB, easy) {
+            if (easy) {
+                ctx.strokeStyle = "rgba(255, 255, 255, 0.3)";
+            } else {
+                ctx.strokeStyle = "rgba(255, 100, 100, 0.1)";
             }
+            
+            ctx.beginPath();
+            ctx.moveTo(starA.sprite.ox, starA.sprite.oy);
+            ctx.lineTo(starB.sprite.ox, starB.sprite.oy);
+            ctx.stroke();
+        }
 
-            var j, dist,
-                prop = universe.player ? universe.player.tech.propulsion.value : 4/8,
-                next_prop = prop + (1/8); // speed and distance units are in 1/8th of ly           
+        function drawPaths (universe, map) {
+                debug('drawPaths', arguments);
+                var stars       = universe.galaxy.stars,
+                    prop        = universe.player ? universe.player.tech.propulsion.value : 4/8,
+                    next_prop   = prop + (1/8), // speed and distance units are in 1/8th of ly
+                    ctx         = map.context;
 
-            map.context.lineWidth = 4 * map.pixelRatio;
+                ctx.lineWidth = 4 * map.pixelRatio;
 
-            // TODO: Make more efficient. This is basically collision detection
-            for (i in stars) if (stars.hasOwnProperty(i)) {
-                map.drawSprite(stars[i].sprite);
-                for (j in stars) if (stars.hasOwnProperty(j)) {
-                    dist = universe.distance(stars[i].x, stars[i].y, stars[j].x, stars[j].y);
-                    if (dist <= next_prop) {
-                        if (dist <= prop) {
-                            map.context.strokeStyle = "rgba(255, 255, 255, 0.3)";
-                        } else {
-                            map.context.strokeStyle = "rgba(255, 100, 100, 0.1)";
+                var done = []; // efficiency hack because stars is an object.
+
+                // TODO: Make more efficient. This is basically collision detection
+                for (var i in stars) if (stars.hasOwnProperty(i)) {
+                    var starA = stars[i];
+                    done[i] = true;
+
+                    for (var j in stars) if (stars.hasOwnProperty(j)) {
+                        var starB = stars[j];
+                        if (starA === starB || done[j]) {
+                            continue;
                         }
-                           
-                        map.context.beginPath();
-                        map.context.moveTo(stars[i].sprite.ox, stars[i].sprite.oy);
-                        map.context.lineTo(stars[j].sprite.ox, stars[j].sprite.oy);
-                        map.context.stroke();
+                        if (Math.abs(starA.x - starB.x) > next_prop || Math.abs(starA.y - starB.y) > next_prop) {
+                            continue;
+                        }
+                        var dist = universe.distance(starA.x, starA.y, starB.x, starB.y);
+                        if (dist <= next_prop) {
+                            drawPath(ctx, starA, starB, dist <= prop);
+                        }
                     }
                 }
-            }
-        });
-        var waypoint_patch = patch(function () {
-            debug('clicked star', event, data);
-            // right click from fleet screen
-            if (npui.showingScreen === "fleet" &&
-                    data.buttons === 2 && data.button === 2) {
-                debug('go go go ');
-                return;
-            }
-            return;
-        });
+        }
 
-        var init_patch = patch(function (settings, universe, inbox, npui, npuis, np, si) {
-            debug('universe', universe);
-            debug('inbox   ', inbox);
-            debug('npui    ', npui);
-            debug('npuis   ', npuis);
-            debug('np      ', np);
-            debug('settings', settings);
+        function drawBoundaries (universe, map) {
+            var players = universe.galaxy.players;
+
+            for (var i in players)  if (players.hasOwnProperty(i)) {
+                drawPlayerBoundary(universe, map, players[i]);
+            }
+        }
+
+        function drawPlayerBoundary (universe, map, player) {
+                var map_ctx         = map.context,
+                    map_canvas      = map_ctx.canvas,
+                    stars           = universe.galaxy.stars,
+                    buffer          = canvas(map_canvas.width, map_canvas.height),
+                    ctx             = buffer.getContext('2d');
+
+                // draw scanning ranges
+                for (var j in stars)  if (stars.hasOwnProperty(j)) {
+                    if (stars[j].player !== player) {
+                        continue;
+                    }
+                    var star    = stars[j],
+                        tx      = (star.x * map.scale + map.sx) * map.pixelRatio,
+                        ty      = (star.y * map.scale + map.sy) * map.pixelRatio,
+                        sc      = (star.player.tech.scanning.value) * map.scale * map.pixelRatio / 250;
+                    
+                    // so original code will draw on our buffer
+                    map.context = ctx;
+                    map.drawSprite({
+                            ox: tx,
+                            oy: ty,
+                            width: 576,
+                            height: 576,
+                            pivotX: 288,
+                            pivotY: 288,
+                            rotation: 0,
+                            scale: sc,
+                            image: tint(map.scanningRangeSrc, player.color),
+                            spriteX: 0,
+                            spriteY: 0,
+                            visible: true});
+                    map.context = map_ctx;
+                }
+
+                for (var i in stars) if (stars.hasOwnProperty(i)) {
+                    if (stars[i].player !== player) {
+                        continue;
+                    }
+                    var star    = stars[i],
+                        x       = star.sprite.ox,
+                        y       = star.sprite.oy,
+                        radius  = (star.player.tech.scanning.value) * map.scale * 0.95;
+
+                        clearCircle(ctx, x, y, radius);
+                }
+
+                // draw buffer
+                map_ctx.globalCompositeOperation = 'source-over';
+                map_ctx.drawImage(buffer, 0, 0);
+        }
+
+
+        w._init = function (Mousetrap, Crux, NeptunesPride, universe, inbox, npui, npuis, np, si) {
+            debug('_init args: ', arguments);
 
             np.onStartPaths = function (e) {
+                debug("onStartPaths");
                 if (universe.editMode === "show_paths") {
                     universe.editMode = "normal";
                 } else {
@@ -177,29 +243,57 @@ try {
             };
             
             np.on("start_paths", np.onStartPaths);
-            Mousetrap.bind(["p", "P"], function () {np.trigger("start_paths");});
+            Mousetrap.bind("p", caller(np.trigger, "start_paths"));
+
+
+            np.onStartBoundaries = function (e) {
+                debug("onStartBoundaries");
+                if (universe.editMode === "show_boundaries") {
+                    universe.editMode = "normal";
+                } else {
+                    universe.editMode = "show_boundaries";
+                    np.trigger("hide_screen");
+                }
+                np.trigger("map_refresh");
+            };
+            
+            np.on("start_boundaries", np.onStartBoundaries);
+            Mousetrap.bind("b", caller(np.trigger, "start_boundaries"));
+
 
             document.oncontextmenu = function (e) {
                     e.preventDefault();
                     e.stopPropagation();
             };
-            // map.on("contextmenu", function (e) {
-            //         e.preventDefault();
-            //         e.stopPropagation();
-            // });
 
-        });
+            NeptunesPride.Map = wrap(NeptunesPride.Map, function (args) {
+                debug('NP.Map args', args); //npui, universe
+                return args;
+            }, function (args, map) {
+                w.map = map;
 
+                debug('wrapping map.draw');
+                map.draw = wrap(map.draw, function (args) {
+                    return args;
+                }, function (args, ret) {
 
-        function patch_init (src, content) {
-            debug("patching init");
-            var patched_source = patch_script(content,
-                "var si = NeptunesPride.SharedInterface(npui);",
-                "",
-                init_patch);
-            insert_script(patched_source);
-        }
-        disable_script(/\$\(window\)\.ready\(function \(\) \{/, patch_init);
+                    if (universe.editMode === "show_boundaries") {
+                        drawBoundaries(universe, map);
+                    }
+                    else if (universe.editMode === "show_paths") {
+                        drawPaths(universe, map);
+                    }
+
+                    return ret;
+                });
+                // reregister draw function
+                Crux.tickCallbacks.pop();
+                Crux.tickCallbacks.push(map.draw);
+                return map;
+            });
+        };
+
+        inject_init();
 
     }) (unsafeWindow);
 } catch (e) {
